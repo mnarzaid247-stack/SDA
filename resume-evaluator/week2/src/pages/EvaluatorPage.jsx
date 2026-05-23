7ee48a6e66bf229090f5f937306b836bc51a67c9
manar <mnarzaid247@gmail.com>
2026-05-23 11:26:08 +0300

    adding a useState const

--- resume-evaluator/week2/src/pages/EvaluatorPage.jsx
+++ resume-evaluator/week2/src/pages/EvaluatorPage.jsx
@@ -5,19 +5,53 @@
 //             add conditional rendering to the results area
 // TODO Day 5: move all state and logic to src/hooks/useEvaluator.js
 //             import and use the hook here instead
-
+import { useState } from "react"
 export default function EvaluatorPage() {
+  const [jobDescription, setJobDescription] = useState('')
+  const [prompt, setPrompt] = useState('')
+  const [file, setFile] = useState(null)
+  const [status, setStatus] = useState('idle')
+
+  function onSubmit(e) {
+    e.preventDefault()
+    if(!jobDescription || !file) {
+      setStatus('error')
+      return
+    }
+    setStatus('loading')
+  }
+
   return (
     <main>
       <section className="form-section">
         <h2>Evaluate a Resume</h2>
-        <form method="post" id="form">
-          <label for="job"> job description</label>
-          <textarea rows="2" cols="10" name="job" id="job"></textarea>
-          <label for="prompt">Custom Prompt</label>
-          <textarea rows="2" cols="60" name="prompt" id="prompt"></textarea>
-          <label for="cv">Upload your CV</label>
-          <input type="file" accept=".pdf" name="cv" id="cv"></input>
+        <form method="post" id="form" onSubmit={onSubmit}>
+          <label htmlFor="job"> job description</label>
+          <textarea
+          rows="2"
+          cols="10" 
+          name="job" 
+          id="job" 
+          value={jobDescription}
+          onChange={(e) => setJobDescription(e.target.value)}
+          ></textarea>
+          <label htmlFor="prompt">Custom Prompt</label>
+          <textarea
+           rows="2" 
+           cols="60" 
+           name="prompt" 
+           id="prompt"
+           value={prompt}
+           onChange={(e) => setPrompt(e.target.value)}
+           ></textarea>
+          <label htmlFor="cv">Upload your CV</label>
+          <input
+          type="file" 
+          accept=".pdf" 
+          name="cv" 
+          id="cv"
+          onChange={(e) => setFile(e.target.files[0] || null)}
+          />
           <button type="submit"> Submit</button>
         </form>
       </section>
