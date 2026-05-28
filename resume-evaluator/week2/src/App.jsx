2c10d310ae6fac42b3583fb9f28c44ca79195fbc
mnarzaid247-stack <mnarzaid247@gmail.com>
2026-05-29 02:54:47 +0300

    Update App.jsx

--- resume-evaluator/week2/src/App.jsx
+++ resume-evaluator/week2/src/App.jsx
@@ -1,14 +1,20 @@
 import Header from './components/Header'
 import EvaluatorPage from './pages/EvaluatorPage'
-// TODO Day 4: import Routes, Route from react-router-dom
-//             import LoginPage and RegisterPage
-//             add route definitions
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <div>
-      <Header title="Resume Evaluator"/>
-      <EvaluatorPage/>
+      <Header title="Resume Evaluator" />
+
+      <Routes>
+        <Route path="/" element={<EvaluatorPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<h2>Page not found</h2>} />
+      </Routes>
     </div>
   )
 }
